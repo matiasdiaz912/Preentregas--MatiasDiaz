0203c17840fc051fcf1a661fa5448538c9a6c05e
matidiaz <matiasdiazbarrera@gmail.com>
2023-10-02 20:18:46 -0300

    Reentrega

--- Proyecto Final MatiasDiaz/carrito.js
+++ Proyecto Final MatiasDiaz/carrito.js
@@ -1,35 +1,96 @@
-export const todosLosProductos = [
-    {id: 1,Nombre: "Celular Samsung Galaxy A04e 64/3gb Copper - Multipoint", Precio: 80000, imagen: "./img/img-main1.jpg",  Marca: "Samsung", Categoria: "Celulares", cantidad: 1},
-    {id: 2,Nombre: "Iphone 14 Max pro ||  12 y 18 cuotas sin interes", Precio: 740000 , imagen: "./img/img.main2.jpg",  Marca: "Apple", Categoria: "Celulares", cantidad: 1},
-    {id: 3,Nombre: "Celular Motorola E13 64GB Natural | Cuotas sin interes", Precio: 75000 , imagen: "./img/img.main3.jpg",  Marca: "Motorola", Categoria: "Celulares", cantidad: 1},
-    {id: 4,Nombre: "Heladeras Samsung: la gama completa | Samsung Argentina", Precio: 300000 , imagen: "./img/img.main6.avif",  Marca: "Samsung", Categoria: "Heladeras", cantidad: 1},
-    {id: 5,Nombre: "HELADERA DREAN 364 LTRS BLANCA (HDR370F11S)", Precio: 255000 , imagen: "./img/img-main4.jpg",  Marca: "Sbs Dream", Categoria: "Heladeras", cantidad: 1},
-    {id: 6,Nombre: "Heladera SBS Drean YEP26FGTFSS 719 Lts Inoxidable", Precio: 750000 , imagen: "./img/img.main5.jpg",  Marca: "Sbs Dream", Categoria: "Heladeras", cantidad: 1},
-    {id: 7,Nombre: "Tablets Smart Tab Serie Yoga | 12 cuotas sin interes", Precio: 200000 ,  imagen: "./img/img-main7.jpg",  Marca: "Smart Tab", Categoria: "Mas Productos", cantidad: 1},
-    {id: 8,Nombre: "Tablet Samsung Galaxy Tab S6 Lite 10.4 64gb Y 4gb Ram Nox",Precio: 150000 , imagen: "./img/img-main8.webp",  Marca: "Samsung", Categoria: "Mas Productos", cantidad: 1},
-    {id: 9,Nombre: "Licuadora de Pie Oster 1,5Lts 3 Velocidades BLSTPYG1211NBG", Precio: 45000 , imagen: "./img/img-main9.jpg",  Marca: "Oster", Categoria: "Mas Productos", cantidad: 1},
-    {id: 10,Nombre: "Licuadora Midea 800W INOX | MIDEA | cuotas sin interes",Precio: 30000, imagen: "./img/img-main10.jpg",  Marca: "Midea", Categoria: "Mas Productos", cantidad: 1},
-    {id: 11,Nombre: "Batidora Amasadora Philips HR7912/90 || cuotas s/ interes", Precio: 60000, imagen: "./img/batidoras.jpg",  Marca: "Philips", Categoria: "Mas Productos", cantidad: 1},
-    {id: 12,Nombre: "Batidora Amasadora Philips H832947890 || cuotas s/interes",Precio: 60000, imagen: "./img/batidoras1.jpg",  Marca: "Philips", Categoria: "Mas Productos", cantidad: 1},
-]
+// const todosLosProductos = [
+//     {id: 1,Nombre: "Celular Samsung Galaxy A04e 64/3gb Copper - Multipoint", Precio: 80000, imagen: "./img/img-main1.jpg",  Marca: "Samsung", Categoria: "Celulares", cantidad: 1},
+//     {id: 2,Nombre: "Iphone 14 Max pro ||  12 y 18 cuotas sin interes", Precio: 740000 , imagen: "./img/img.main2.jpg",  Marca: "Apple", Categoria: "Celulares", cantidad: 1},
+//     {id: 3,Nombre: "Celular Motorola E13 64GB Natural | Cuotas sin interes", Precio: 75000 , imagen: "./img/img.main3.jpg",  Marca: "Motorola", Categoria: "Celulares", cantidad: 1},
+//     {id: 4,Nombre: "Heladeras Samsung: la gama completa | Samsung Argentina", Precio: 300000 , imagen: "./img/img.main6.avif",  Marca: "Samsung", Categoria: "Heladeras", cantidad: 1},
+//     {id: 5,Nombre: "HELADERA DREAN 364 LTRS BLANCA (HDR370F11S)", Precio: 255000 , imagen: "./img/img-main4.jpg",  Marca: "Sbs Dream", Categoria: "Heladeras", cantidad: 1},
+//     {id: 6,Nombre: "Heladera SBS Drean YEP26FGTFSS 719 Lts Inoxidable", Precio: 750000 , imagen: "./img/img.main5.jpg",  Marca: "Sbs Dream", Categoria: "Heladeras", cantidad: 1},
+//     {id: 7,Nombre: "Tablets Smart Tab Serie Yoga | 12 cuotas sin interes", Precio: 200000 ,  imagen: "./img/img-main7.jpg",  Marca: "Smart Tab", Categoria: "Mas Productos", cantidad: 1},
+//     {id: 8,Nombre: "Tablet Samsung Galaxy Tab S6 Lite 10.4 64gb Y 4gb Ram Nox",Precio: 150000 , imagen: "./img/img-main8.webp",  Marca: "Samsung", Categoria: "Mas Productos", cantidad: 1},
+//     {id: 9,Nombre: "Licuadora de Pie Oster 1,5Lts 3 Velocidades BLSTPYG1211NBG", Precio: 45000 , imagen: "./img/img-main9.jpg",  Marca: "Oster", Categoria: "Mas Productos", cantidad: 1},
+//     {id: 10,Nombre: "Licuadora Midea 800W INOX | MIDEA | cuotas sin interes",Precio: 30000, imagen: "./img/img-main10.jpg",  Marca: "Midea", Categoria: "Mas Productos", cantidad: 1},
+//     {id: 11,Nombre: "Batidora Amasadora Philips HR7912/90 || cuotas s/ interes", Precio: 60000, imagen: "./img/batidoras.jpg",  Marca: "Philips", Categoria: "Mas Productos", cantidad: 1},
+//     {id: 12,Nombre: "Batidora Amasadora Philips H832947890 || cuotas s/interes",Precio: 60000, imagen: "./img/batidoras1.jpg",  Marca: "Philips", Categoria: "Mas Productos", cantidad: 1},
+// ]
 
 
 
-let carritoJS = document.getElementById("carritoJS")
+
 let desplegarCarrito = document.getElementById("desplegarCarrito")
 let PrecioFinal = document.getElementById("PrecioFinal")
+let productos = document.getElementById("productos")
 let botonBasura;
 let botonEliminar;
-let productos = document.getElementById("productos")
 let botones;
+let precioTotal;
+let botonComprar;
+import { traerMonedero } from "./main.js";
+import { monederoEnCarrito } from "./main.js";
+import { desplazarProductos } from "./main.js";
 export let carritoLleno;
 export let botonesCarrito = document.getElementById("btn-carrito")
 export let carritoEnCSS = document.getElementById("carritoenCSS")
 
- let carritoCargado = []
+///////////////////////////////////////////////////////////////////////////////////// ////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////// ////////////////////////////////////////////////////////////////////////
 
+ let carritoCargado = []
+let productosToTheBasket = JSON.parse(localStorage.getItem("Productos del carrito"))
+
+let productosCargados;
+
+if(!productosCargados){
+    localStorage.setItem("Productos del carrito", JSON.stringify(productosToTheBasket))
+    productosCargados = productosToTheBasket
+}else{
+    productosCargados = productosToTheBasket
+}
  
- function recorrerArrayProductos (array){
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+ if(productosCargados === null || productosCargados.length == 0){
+
+         let divCarrito = document.createElement("div")
+        divCarrito.innerHTML = `<h2 class="carrito--vacio">El carrito esta vacio</h2>`
+       carritoEnCSS.append(divCarrito)
+        
+}else{
+
+        carritoLleno = document.createElement("div")
+            carritoLleno.classList.add("carrito-lleno")
+            carritoLleno.innerHTML = `<i class="bi bi-exclamation-lg"></i>`
+            desplegarCarrito.append(carritoLleno)
+
+
+         productosCargados.forEach(item => {
+            let carritoConProductos = document.createElement("div")
+            carritoConProductos.classList.add("carritoEnCss")
+    
+            carritoConProductos.innerHTML = `<img src="${item.imagen}" alt="${item.Nombre}">
+    
+            <div class="parrafo-carrito"> 
+                <h2>${item.Nombre}</h2>
+                <p>$${item.Precio}</p>
+                <p>Cantidad: ${item.cantidad}</p>
+            </div>
+                <button class="trash" id="${item.id}"">
+                <i class="bi bi-trash3"></i>
+            </button>`
+    
+            carritoEnCSS.append(carritoConProductos)
+    
+        })
+
+         eliminarProductosCarrito()
+         botonesEnCarrito()
+    }
+    
+ 
+
+ ///////////////////////////////////////////////////////////////////////////////////// ////////////////////////////////////////////////////////////////////////
+ ///////////////////////////////////////////////////////////////////////////////////// ////////////////////////////////////////////////////////////////////////
+ 
+ export function recorrerArrayProductos (array){
    
     productos.innerHTML = ""
     array.forEach((item) => {
@@ -48,22 +109,39 @@ export let carritoEnCSS = document.getElementById("carritoenCSS")
      productosAlCarrito()
 }
 
-    recorrerArrayProductos(todosLosProductos)
-     localStorage.setItem("Productos", JSON.stringify(todosLosProductos))
 
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
  function productosAlCarrito (){
     botones.forEach(item => {
         item.addEventListener("click", (e) => {
 
             let boton = e.currentTarget.id
-
             const productoElegido = todosLosProductos.find(producto => producto.id == boton)
+        
            
-            carritoCargado.push(productoElegido)
-           
-           localStorage.setItem("Productos del carrito", JSON.stringify(carritoCargado))         
+            if(productosCargados.some(producto => producto.id == boton)){
+                let productoElegido2 = productosCargados.findIndex(producto => producto.id == boton)
+               productosCargados[productoElegido2].cantidad++
+                productosCargados.cantidad * productosCargados.precio
+                localStorage.setItem("Productos del carrito", JSON.stringify(productosCargados))  
+              
+               
+              }else{
+                productosCargados.cantidad = 1
+                    productosCargados.push(productoElegido)
+                    localStorage.setItem("Productos del carrito", JSON.stringify(productosCargados)) 
+                 
+              }
+                 
+           productosCargados = JSON.parse(localStorage.getItem("Productos del carrito")) 
+
+           if(!carritoLleno){
+            carritoLleno = document.createElement("div")
+            carritoLleno.classList.add("carrito-lleno")
+            carritoLleno.innerHTML = `<i class="bi bi-exclamation-lg"></i>`
+            }
+
+            carritoActualizado()
+            botonesEnCarrito()
 
            Toastify({
             text: "Producto Agregado",
@@ -80,27 +158,33 @@ export let carritoEnCSS = document.getElementById("carritoenCSS")
 
 }
 
-let productosCargados = JSON.parse(localStorage.getItem("Productos del carrito")) 
+// recorrerArrayProductos(todosLosProductos)   
 
+fetch("productos.json")
+.then(response => response.json())
+.then(data => 
+    recorrerArrayProductos(data))
 
-// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-   function carritoActualizado(){
+ productosCargados = JSON.parse(localStorage.getItem("Productos del carrito")) 
+localStorage.setItem("Productos", JSON.stringify(todosLosProductos))
 
+///////////////////////////////////////////////////////////////////////////////////// ////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////// ////////////////////////////////////////////////////////////////////////
+
+ 
+function carritoActualizado(){
    if(productosCargados === null || productosCargados.length == 0){
+
+    carritoEnCSS.innerHTML = ""
     
-        carritoJS.addEventListener("click", () => {
+    
         let div = document.createElement("div")
         div.innerHTML = `<h2 class="carrito--vacio">El carrito esta vacio</h2>`
-        desplegarCarrito.append(div)
-        })
+        carritoEnCSS.append(div)
+        
     
     
     }else{
-    
-        carritoLleno = document.createElement("div")
-        carritoLleno.classList.add("carrito-lleno")
-        carritoLleno.innerHTML = `<i class="bi bi-exclamation-lg"></i>`
-       desplegarCarrito.append(carritoLleno)
 
        carritoEnCSS.innerHTML = ""
     
@@ -120,69 +204,103 @@ let productosCargados = JSON.parse(localStorage.getItem("Productos del carrito")
         </button>`
 
         carritoEnCSS.append(carritoConProductos)
-    })
 
+    })
     
-    
-    //////////////////////////////////////////////////////////////////////////////////////////////// Parte Final delCarrito ////////////////////////////
+}
+   //////////////////////////////////////////////////////////////////////////////////////////////// Parte Final delCarrito ////////////////////////////}
+
+       
+        eliminarProductosCarrito()
 }
 
-botonBasura = document.querySelectorAll(".trash")
-eliminarProductosCarrito()
-}
 
 
-carritoActualizado()
 
-let precioTotal;
-let botonComprar;
 
-if(productosCargados != null || productosCargados.length > 0){
+function botonesEnCarrito (){
 
-     precioTotal = document.createElement("p")
-    let total = productosCargados.reduce((acumulador, item) => acumulador + item.Precio, 0)
+if(productosCargados === null || productosCargados.length == 0){
+
+    PrecioFinal.innerHTML = ""
+    botonesCarrito.innerHTML = ""
+    carritoActualizado()
     
-    precioTotal.innerHTML = `Precio Total: $${total}`
-    precioTotal.classList.add("precioFinal")
-    PrecioFinal.append(precioTotal)
-    
-    botonComprar = document.createElement("button")
-    botonComprar.innerHTML = "Comprar"
-    botonComprar.classList.add("botonCompras")
-    botonesCarrito.append(botonComprar)
-    
-    
-    botonEliminar = document.createElement("button")
-    botonEliminar.innerHTML = "Limpiar Carrito"
-    botonEliminar.classList.add("botonCompras")
-    botonesCarrito.append(botonEliminar)
-    
-    botonComprar.addEventListener("click", () =>{
-        
-        localStorage.removeItem("Productos del carrito")
-    
-        Swal.fire({
-            icon: 'success',
-            title: 'Exitosa',
-            text: 'Tu compra fue realizada con exito!',
-          })
-    })
-    
-    botonEliminar.addEventListener("click", () =>{
-        localStorage.removeItem("Productos del carrito")
-       location.reload()
-    })
 }else{
-    precioTotal.className = "display-none"
-    botonComprar.className = "display-none"
-    botonEliminar.className = "display-none"
+
+   PrecioFinal.innerHTML = ""
+    botonesCarrito.innerHTML = ""
+
+    precioTotal = document.createElement("p")
+   let total = productosCargados.reduce((acumulador, item) => acumulador + item.Precio * item.cantidad, 0)
+   
+   precioTotal.innerHTML = `Precio Total: $${total}`
+   precioTotal.classList.add("precioFinal")
+   PrecioFinal.append(precioTotal)
+   
+   botonComprar = document.createElement("button")
+   botonComprar.innerHTML = "Comprar"
+   botonComprar.classList.add("botonCompras")
+   botonesCarrito.append(botonComprar)
+   
+   
+   botonEliminar = document.createElement("button")
+   botonEliminar.innerHTML = "Limpiar Carrito"
+   botonEliminar.classList.add("botonCompras")
+   botonesCarrito.append(botonEliminar)
+   
+   botonComprar.addEventListener("click", () =>{
+
+    if(traerMonedero > total){
+        productosCargados.splice(0, productosCargados.length)
+        carritoCargado.splice(0, carritoCargado.length)
+        localStorage.setItem("Productos del carrito", JSON.stringify(productosCargados))
+    
+           carritoActualizado()
+           botonesEnCarrito()
+       
+           Swal.fire({
+               icon: 'success',
+               title: 'Exitosa',
+               text: 'Tu compra fue realizada con exito!',
+             })
+
+             let precioTotal2 = traerMonedero - total
+             monederoEnCarrito.innerHTML = `Monedero: $${precioTotal2}`
+             monederoEnCarrito.classList.add("display-none")
+             carritoSuperior.append(monederoEnCarrito)
+
+             desplegarCarrito.className = "display-none"
+            desplazarProductos.className = "sinPadding"
+            monederoEnCarrito.className = "display-none"
+
+    }else{
+        Swal.fire({
+            icon: 'warning',
+            title: 'Error',
+            text: 'Superaste el precio del monedero',
+          })
+    }
+       
+   
+   })
+   
+   
+   botonEliminar.addEventListener("click", () =>{
+ productosCargados.splice(0, productosCargados.length)
+ localStorage.setItem("Productos del carrito", JSON.stringify(productosCargados))
+    carritoActualizado()
+    botonesEnCarrito()
+})
+
 }
-
-
+}
 
     ///////////////////////////////////////////////////////////////////////////// Eliminar Productos del carrito /////////////////////////////////////////////////////
 
 function eliminarProductosCarrito (){
+
+    botonBasura = document.querySelectorAll(".trash")
     botonBasura.forEach(item => {
         item.addEventListener("click", (e) => {
            
@@ -192,6 +310,7 @@ function eliminarProductosCarrito (){
             productosCargados.splice(productoElegido, 1)
            
             carritoActualizado()
+            botonesEnCarrito()
 
             localStorage.setItem("Productos del carrito", JSON.stringify(productosCargados))
 
@@ -208,4 +327,3 @@ function eliminarProductosCarrito (){
     })
 }
 
-console.log(productosCargados);
--- Proyecto Final MatiasDiaz/main.js
+++ Proyecto Final MatiasDiaz/main.js
@@ -9,21 +9,51 @@ let carritoJS = document.getElementById("carritoJS")
 let activarHover = document.querySelectorAll(".active-hover-js")
 let tituloVariado = document.getElementById("titulo-variado")
 let cerrarCarrito = document.getElementById("cerrar-carrito")
-let desplazarProductos = document.getElementById("desplazar--productos")
+let monedero = document.getElementById("monedero")
+let cartelMonedero = document.getElementById("div--monedero")
+let sacarCartelMonedero = document.getElementById("sacar--cartel--monedero")
+let enviarMonedero = document.getElementById("enviar-monedero")
+let parrafoAbajo = document.getElementById("parrafoAbajo")
+let carritoSuperior = document.getElementById("carritoSuperior")
+
+const todosLosProductos = [
+    {id: 1,Nombre: "Celular Samsung Galaxy A04e 64/3gb Copper - Multipoint", Precio: 80000, imagen: "./img/img-main1.jpg",  Marca: "Samsung", Categoria: "Celulares", cantidad: 1},
+    {id: 2,Nombre: "Iphone 14 Max pro ||  12 y 18 cuotas sin interes", Precio: 740000 , imagen: "./img/img.main2.jpg",  Marca: "Apple", Categoria: "Celulares", cantidad: 1},
+    {id: 3,Nombre: "Celular Motorola E13 64GB Natural | Cuotas sin interes", Precio: 75000 , imagen: "./img/img.main3.jpg",  Marca: "Motorola", Categoria: "Celulares", cantidad: 1},
+    {id: 4,Nombre: "Heladeras Samsung: la gama completa | Samsung Argentina", Precio: 300000 , imagen: "./img/img.main6.avif",  Marca: "Samsung", Categoria: "Heladeras", cantidad: 1},
+    {id: 5,Nombre: "HELADERA DREAN 364 LTRS BLANCA (HDR370F11S)", Precio: 255000 , imagen: "./img/img-main4.jpg",  Marca: "Sbs Dream", Categoria: "Heladeras", cantidad: 1},
+    {id: 6,Nombre: "Heladera SBS Drean YEP26FGTFSS 719 Lts Inoxidable", Precio: 750000 , imagen: "./img/img.main5.jpg",  Marca: "Sbs Dream", Categoria: "Heladeras", cantidad: 1},
+    {id: 7,Nombre: "Tablets Smart Tab Serie Yoga | 12 cuotas sin interes", Precio: 200000 ,  imagen: "./img/img-main7.jpg",  Marca: "Smart Tab", Categoria: "Mas Productos", cantidad: 1},
+    {id: 8,Nombre: "Tablet Samsung Galaxy Tab S6 Lite 10.4 64gb Y 4gb Ram Nox",Precio: 150000 , imagen: "./img/img-main8.webp",  Marca: "Samsung", Categoria: "Mas Productos", cantidad: 1},
+    {id: 9,Nombre: "Licuadora de Pie Oster 1,5Lts 3 Velocidades BLSTPYG1211NBG", Precio: 45000 , imagen: "./img/img-main9.jpg",  Marca: "Oster", Categoria: "Mas Productos", cantidad: 1},
+    {id: 10,Nombre: "Licuadora Midea 800W INOX | MIDEA | cuotas sin interes",Precio: 30000, imagen: "./img/img-main10.jpg",  Marca: "Midea", Categoria: "Mas Productos", cantidad: 1},
+    {id: 11,Nombre: "Batidora Amasadora Philips HR7912/90 || cuotas s/ interes", Precio: 60000, imagen: "./img/batidoras.jpg",  Marca: "Philips", Categoria: "Mas Productos", cantidad: 1},
+    {id: 12,Nombre: "Batidora Amasadora Philips H832947890 || cuotas s/interes",Precio: 60000, imagen: "./img/batidoras1.jpg",  Marca: "Philips", Categoria: "Mas Productos", cantidad: 1},
+]
 
 
-import { todosLosProductos } from "./carrito.js";
+// import { todosLosProductos } from "./carrito.js";
 import { carritoEnCSS } from "./carrito.js";
 import { botonesCarrito } from "./carrito.js";
 import { carritoLleno } from "./carrito.js";
+import { recorrerArrayProductos } from "./carrito.js";
 
 
 //////////////////////////////////////////////////////////////////////Empieza Modo Oscuro/////////////////////////////////////////////////////////////////////////////////////////
+export let traerMonedero = localStorage.getItem("Monedero")
+export let monederoEnCarrito = document.createElement("div")
+export let desplazarProductos = document.getElementById("desplazar--productos")
+
+monederoEnCarrito.innerHTML = `Monedero: $${traerMonedero}`
+monederoEnCarrito.classList.add("display-none")
+carritoSuperior.append(monederoEnCarrito)
+
 
 function activarModoOscuro (){
     body.classList.add("modo-oscuro")
     cambiarModo.innerHTML =  `<button class="boton-modos"><i class="bi bi-sun-fill colorClaro"></i></button>`
    carritoJS.classList.add("colorClaro")
+   monedero.className = "walletClara"
     localStorage.setItem("modo-oscuro", "activado")
 }
 
@@ -32,6 +62,7 @@ function desactivarModoOscuro (){
      cambiarModo.innerHTML = ""
      cambiarModo.innerHTML =  `<button class="boton-modos"><i class="bi bi-moon-fill"></i></button>`
      carritoJS.classList.remove("colorClaro")
+    monedero.className = "wallet"
     localStorage.setItem("modo-oscuro", "desactivado")
 }
 
@@ -47,8 +78,8 @@ fondoPermanente === "activado" ? activarModoOscuro() : desactivarModoOscuro()
 ///////////////////////////////////////////////////////////////////////Termina Modo Oscuro//////////////////////////////////////////////////////////////////////////////////////
 
 
-const noMostrar = () => {
-    eliminarCartel.className = "display-none"
+const noMostrar = (divEliminar) => {
+    divEliminar.className = "display-none"
 }
 
 let traerNombreDelLs = localStorage.getItem("Nombre")
@@ -61,17 +92,53 @@ procesarNombre.addEventListener("submit", (e) => {
    location.reload()
 })
 
-sacarCartel.addEventListener("click", noMostrar)
+sacarCartel.addEventListener("click", () => noMostrar(eliminarCartel))
 
 
 if(traerNombreDelLs === "" || traerNombreDelLs.includes("@")){
     parrafoDeAbajo.innerText = "Error. Su nombre no ha sido registrado"
 }else{
-    noMostrar()
+    noMostrar(eliminarCartel)
     bienvenida.innerText = `Bienvenido ${traerNombreDelLs}!`
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////// Monedero ////////////////////////////////////////////////////////////////////////////////////
+monedero.addEventListener("click", () => {
+    cartelMonedero.className = "cartel-monedero"
+})
+
+
+sacarCartelMonedero.addEventListener("click", () => noMostrar(cartelMonedero))
+
+
+function actualizarMonedero (){
+
+
+    enviarMonedero.addEventListener("submit", (e) => {
+
+        e.preventDefault()
+        let inputs = e.target.children
+
+        if (inputs[0].value <= 0 || inputs[0].value === undefined || inputs[0].value === null){
+            cartelMonedero.className = "cartel-monedero"
+           parrafoAbajo.innerText = "El monedero no puede ser menor que 1"
+        
+        }else{
+
+            noMostrar(cartelMonedero)
+            localStorage.setItem("Monedero", inputs[0].value)
+            traerMonedero = localStorage.getItem("Monedero")
+    
+           monederoEnCarrito.classList.add("display-none")
+            monederoEnCarrito.innerHTML = `Monedero: $${traerMonedero}`
+            carritoSuperior.append(monederoEnCarrito)
+        }
+    })
+
+}
+
+actualizarMonedero()
+
 
 ////////////////////////////////////////////////////////////////////////////////////Nav/////////////////////////////////////////////////////////////////////
 
@@ -85,6 +152,7 @@ activarHover.forEach(item => {
 
         if(e.currentTarget.id != "todos-completo"){
             let productosPorBoton = todosLosProductos.filter((producto) => producto.Categoria === e.currentTarget.id)
+
           
         recorrerArrayProductos(productosPorBoton)
             tituloVariado.innerHTML = productosPorBoton[0].Categoria
@@ -100,6 +168,7 @@ activarHover.forEach(item => {
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+
 carritoJS.addEventListener("click", () => {
     desplegarCarrito.className = "carritoDesplegado"
     cerrarCarrito.classList.add("visible")
@@ -107,11 +176,14 @@ carritoJS.addEventListener("click", () => {
     carritoEnCSS.className = ""
     botonesCarrito.className = "botonesCarrito"
     PrecioFinal.className = "precioFinal"
+    monederoEnCarrito.classList.remove("display-none")
+    monederoEnCarrito.className = "monedero--carrito"
     carritoLleno.className = "display-none"
 })
 
 cerrarCarrito.addEventListener("click", () =>{
     desplegarCarrito.className = "display-none"
     desplazarProductos.className = "sinPadding"
-    
+    monederoEnCarrito.className = "display-none"
 })
+
